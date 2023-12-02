cee11060ec8d5587121da1f652a1a96de287e5a9
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-02 23:24:06 +0100

    Create Learning to COUNT().sql

---
+++ DataCampIntermediateSQL/Learning to COUNT().sql
@@ -0,0 +1,11 @@
+/* Learning to COUNT()
+You saw how to use COUNT() in the video. Do you remember what it returns?
+
+Here is a query counting film_id. Select the answer below that correctly describes what the query will return.
+
+SELECT COUNT(film_id) AS count_film_id
+FROM reviews;
+Run the query in the console to test your theory! */
+
+SELECT COUNT(film_id) AS count_film_id
+FROM reviews;
