e19b9f4ad248b4c4d4a3b51e9a4210a709d97e52
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-04 23:57:18 +0100

    Create Order of execution.sql

---
+++ DataCampIntermediateSQL/Order of execution.sql
@@ -0,0 +1,12 @@
+/* Order of execution
+SQL code is processed differently than other programming languages in that you need to let the processor know where to pull the data from before making selections.
+
+It's essential to know your code's order of execution compared to the order it is written in to understand what results you'll get from your query and how to fix any errors that may come up.
+
+Instructions
+
+Drag the SQL keywords into the order that they will be executed in (not the written order) from first (top) to last (bottom). */
+
+FROM
+SELECT
+LIMIT
