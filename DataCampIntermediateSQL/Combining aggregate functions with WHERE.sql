41c936cc9b6f5602e0ef5fd9667e4c562a3e07dd
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-17 00:18:10 +0100

    Update Combining aggregate functions with WHERE.sql

--- DataCampIntermediateSQL/Combining aggregate functions with WHERE.sql
+++ DataCampIntermediateSQL/Combining aggregate functions with WHERE.sql
@@ -5,5 +5,35 @@ This combination is useful when you only want to summarize a subset of your data
 
 Let's see what insights you can gain about the financials in the dataset.
 
+Instructions
+1- Use SUM() to calculate the total gross for all films made in the year 2000 or later, and use the alias total_gross.
+2- Calculate the average amount grossed by all films whose titles start with the letter 'A' and alias with avg_gross_A.
+3- Calculate the lowest gross film in 1994 and use the alias lowest_gross.
+4- Calculate the highest gross film between 2000 and 2012, inclusive, and use the alias highest_gross.
 
 */
+
+-- 1 ---------------------
+-- Calculate the sum of gross from the year 2000 or later
+SELECT SUM(gross) AS total_gross
+FROM films
+WHERE release_year >= 2000;
+
+-- 2 ---------------------
+-- Calculate the average gross of films that start with A
+SELECT AVG(gross) AS avg_gross_A
+FROM films
+WHERE title LIKE 'A%';
+
+-- 3 ---------------------
+-- Calculate the lowest gross film in 1994
+SELECT MIN(gross) AS lowest_gross
+FROM films
+WHERE release_year = 1994;
+-- 4 ---------------------
+-- Calculate the highest gross film released between 2000-2012
+SELECT MAX(gross) AS highest_gross
+FROM films
+WHERE release_year BETWEEN 2000 AND 2012;
+
+
