f0082e5ba9df69ce1c3577244c141b91778169e7
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-16 23:47:55 +0100

    Create Combining aggregate functions with WHERE.sql

---
+++ DataCampIntermediateSQL/Combining aggregate functions with WHERE.sql
@@ -0,0 +1,9 @@
+/* Combining aggregate functions with WHERE
+When combining aggregate functions with WHERE, you get a powerful tool that allows you to get more granular with your insights, for example, to get the total budget of movies made from the year 2010 onwards.
+
+This combination is useful when you only want to summarize a subset of your data. In your film-industry role, as an example, you may like to summarize each certification category to compare how they each perform or if one certification has a higher average budget than another.
+
+Let's see what insights you can gain about the financials in the dataset.
+
+
+*/
