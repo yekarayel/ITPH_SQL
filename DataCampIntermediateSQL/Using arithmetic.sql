2a9dfa38438158c261de13ddcdb72ac39f6b5614
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-19 00:02:25 +0100

    Update Using arithmetic.sql

--- DataCampIntermediateSQL/Using arithmetic.sql
+++ DataCampIntermediateSQL/Using arithmetic.sql
@@ -1,4 +1,9 @@
 /* Using arithmetic
-SQL arithmetic comes in handy when your table is missing a metric you want to review. Suppose you have some data on movie ticket sales, but the table only has fields for ticket price and discount. In that case, you could combine these by subtracting the discount from the ticket price to get the amount the film-goer paid.
+SQL arithmetic comes in handy when your table is missing a metric you want to review. Suppose you have some data on movie ticket sales,
+but the table only has fields for ticket price and discount. In that case, you could combine these by subtracting the discount from 
+the ticket price to get the amount the film-goer paid.
 
-You have seen that SQL can act strangely when dividing integers. What is the result if you divide a discount of two dollars by the paid_price of ten dollars to get the discount percentage?
+You have seen that SQL can act strangely when dividing integers. What is the result if you divide a discount of two dollars by the 
+paid_price of ten dollars to get the discount percentage? */
+
+SELECT (2/10);
