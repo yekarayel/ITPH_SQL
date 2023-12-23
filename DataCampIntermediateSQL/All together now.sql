727db37c54a09e8eadd8dbadea88cb213480b427
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-24 00:11:11 +0100

    Update All together now.sql

--- DataCampIntermediateSQL/All together now.sql
+++ DataCampIntermediateSQL/All together now.sql
@@ -38,4 +38,11 @@ GROUP BY release_year
 HAVING AVG(budget) > 60000000;
 
 -- 4 -----------------------
-
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+HAVING AVG(budget) > 60000000
+-- Order the results from highest to lowest average gross and limit to one
+ORDER BY avg_gross DESC
+LIMIT 1;
