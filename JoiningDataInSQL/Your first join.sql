e3e05f2bb06c6c88ed0ecfe83af8d141a5d7ba73
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-28 23:57:37 +0100

    Update Your first join.sql

--- JoiningDataInSQL/Your first join.sql
+++ JoiningDataInSQL/Your first join.sql
@@ -7,7 +7,20 @@ You can see the different tables in this database to get a sense of what they co
 Click through them and familiarize yourself with the fields that seem to be shared across tables before you continue with the course.
 
 In this exercise, you'll use the cities and countries tables to build your first inner join. You'll start off by selecting 
-all columns in step 1, performing your join in step 2, and then refining your join to choose specific columns in step 3. */
+all columns in step 1, performing your join in step 2, and then refining your join to choose specific columns in step 3. 
 
+Instructions 
+1- Begin by selecting all columns from the cities table, using the SQL shortcut that selects all. 
+2- *Perform an inner join with the cities table on the left and the countries table on the right; do not alias tables here or in 
+the next step.
+  *Identify the relevant column names to join ON by inspecting the cities and countries tabs in the console.
 
+*/
+
+-- 1 ----------------------------
+-- Select all columns from cities
+SELECT *
+FROM cities
+
+-- 2 ----------------------------
 
