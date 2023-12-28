3f33e0db19f6294bdd284e1cc410cba8f4ed3ae4
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-29 00:37:05 +0100

    Update Joining with aliased tables.sql

--- JoiningDataInSQL/Joining with aliased tables.sql
+++ JoiningDataInSQL/Joining with aliased tables.sql
@@ -22,4 +22,10 @@ Instructions
 code from the countries table (aliased as country_code), name, year, and inflation_rate.
 */
 
-
+-- Select fields with aliases
+SELECT c.code AS country_code, c.name, e.year, e.inflation_rate
+FROM countries AS c
+-- Join to economies (alias e)
+INNER JOIN economies AS e 
+-- Match on code field using table aliases
+ON c.code = e.code;
