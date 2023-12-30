d49ec8a2aaf6ef3852a6556057079a576cf48817
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-30 23:58:05 +0100

    Create Inspecting a relationship.sql

---
+++ JoiningDataInSQL/Inspecting a relationship.sql
@@ -0,0 +1,9 @@
+/* Inspecting a relationship
+You've just identified that the countries table has a many-to-many relationship with the languages table. That is, many languages 
+can be spoken in a country, and a language can be spoken in many countries.
+
+This exercise looks at each of these in turn. First, what is the best way to query all the different languages spoken in a country? 
+And second, how is this different from the best way to query all the countries that speak each language?
+
+Recall that when writing joins, many users prefer to write SQL code out of order by writing the join first (along with any table aliases),
+and writing the SELECT statement at the end. */
