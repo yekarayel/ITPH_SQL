638503a8b93b885b2360df41755de1e6f6a13626
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-12-30 23:59:59 +0100

    Update Inspecting a relationship.sql

--- JoiningDataInSQL/Inspecting a relationship.sql
+++ JoiningDataInSQL/Inspecting a relationship.sql
@@ -6,4 +6,21 @@ This exercise looks at each of these in turn. First, what is the best way to que
 And second, how is this different from the best way to query all the countries that speak each language?
 
 Recall that when writing joins, many users prefer to write SQL code out of order by writing the join first (along with any table aliases),
-and writing the SELECT statement at the end. */
+and writing the SELECT statement at the end. 
+
+Instructions 
+
+1- Start with the join statement in line 6; perform an inner join with the countries table as c on the left with the languages table as l on the right.
+Make use of the USING keyword to join on code in line 8.
+Lastly, in line 2, select the country name, aliased as country, and the language name, aliased as language.
+
+*/
+
+-- Select country and language names, aliased
+SELECT c.name AS country, l.name AS language
+-- From countries (aliased)
+FROM countries AS c
+-- Join to languages (aliased)
+INNER JOIN languages AS l
+-- Use code as the joining field with the USING keyword
+USING(code)
