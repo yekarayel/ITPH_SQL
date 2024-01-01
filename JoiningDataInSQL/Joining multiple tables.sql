b3cb1b31c0e741c87b0e291410f2423e645fafaf
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-01 23:50:52 +0100

    Update Joining multiple tables.sql

--- JoiningDataInSQL/Joining multiple tables.sql
+++ JoiningDataInSQL/Joining multiple tables.sql
@@ -19,3 +19,15 @@ FROM countries AS c
 -- Inner join countries and populations, aliased, on code
 INNER JOIN populations AS p
 ON c.code = p.country_code;
+
+-- 2 ------------------------------
+-- Select fields
+SELECT name, e.year, fertility_rate, e.unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+-- Join to economies (as e)
+INNER JOIN economies AS e
+-- Match on country code
+ON c.code = e.code;
+
