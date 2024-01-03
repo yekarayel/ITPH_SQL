a997726dedc6608482334e799496b23992753283
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-03 23:59:04 +0100

    Create This is a LEFT JOIN, right?.sql

---
+++ JoiningDataInSQL/This is a LEFT JOIN, right?.sql
@@ -0,0 +1,13 @@
+/* This is a LEFT JOIN, right?
+Nice work getting to grips with the structure of joins! In this exercise, you'll explore the differences between INNER JOIN and LEFT JOIN. 
+This will help you decide which type of join to use.
+
+As before, you will be using the cities and countries tables.
+
+You'll begin with an INNER JOIN with the cities table (left) and countries table (right). This helps if you are interested only 
+in records where a country is present in both tables.
+
+You'll then change to a LEFT JOIN. This helps if you're interested in returning all countries in the cities table, whether or not 
+they have a match in the countries table.
+
+*/
