eb63314c2f65be9467bf8f9ed6626b68f57a7354
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-04 02:57:44 +0100

    Create Is this RIGHT?.sql

---
+++ JoiningDataInSQL/Is this RIGHT?.sql
@@ -0,0 +1,11 @@
+/* Is this RIGHT?
+You learned that right joins are not used as commonly as left joins. A key reason for this is that right joins can always be re-written 
+as left joins, and because joins are typically typed from left to right, joining from the left feels more intuitive when constructing queries.
+
+It can be tricky to wrap one's head around when left and right joins return equivalent results. You'll explore this in this exercise! 
+
+
+Instructions
+
+Write a new query using RIGHT JOIN that produces an identical result to the LEFT JOIN provided.  */
+
