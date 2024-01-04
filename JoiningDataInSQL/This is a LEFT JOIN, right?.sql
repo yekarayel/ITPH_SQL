b7c73e479921af25f6e7557c2cf1fb1574ca1bb5
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-04 02:33:09 +0100

    Update This is a LEFT JOIN, right?.sql

--- JoiningDataInSQL/This is a LEFT JOIN, right?.sql
+++ JoiningDataInSQL/This is a LEFT JOIN, right?.sql
@@ -14,8 +14,9 @@ Instructions
 1- Perform an inner join with cities AS c1 on the left and countries as c2 on the right.
    Use code as the field to merge your tables on.
 
+2- Change the code to perform a LEFT JOIN instead of an INNER JOIN.
+   After executing this query, have a look at how many records the query result contains.   */
 
-*/
 -- 1 -------------------------
 SELECT 
     c1.name AS city,
@@ -26,5 +27,18 @@ SELECT
 FROM cities AS c1
 -- Perform an inner join with cities as c1 and countries as c2 on country code
 INNER JOIN countries AS c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+
+-- 2 -------------------------
+SELECT 
+	c1.name AS city, 
+    code, 
+    c2.name AS country,
+    region, 
+    city_proper_pop
+FROM cities AS c1
+-- Join right table (with alias)
+LEFT JOIN countries AS c2
 ON c1.country_code = c2.code
 ORDER BY code DESC;
