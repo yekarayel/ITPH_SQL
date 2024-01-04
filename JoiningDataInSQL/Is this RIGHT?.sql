59c51a643be30e2df5d24e68af4768f261700331
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-04 03:01:26 +0100

    Update Is this RIGHT?.sql

--- JoiningDataInSQL/Is this RIGHT?.sql
+++ JoiningDataInSQL/Is this RIGHT?.sql
@@ -9,3 +9,16 @@ Instructions
 
 Write a new query using RIGHT JOIN that produces an identical result to the LEFT JOIN provided.  */
 
+-- Modify this query to use RIGHT JOIN instead of LEFT JOIN
+SELECT countries.name AS country, languages.name AS language, percent
+FROM countries
+LEFT JOIN languages
+USING(code)
+ORDER BY language;
+
+-- Modified query 
+SELECT countries.name AS country, languages.name AS language, percent
+FROM languages
+RIGHT JOIN countries
+USING(code)
+ORDER BY language;
