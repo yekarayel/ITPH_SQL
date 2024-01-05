e154e41d1b176178b75ee1032e5667fbc50596bb
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-05 23:58:27 +0100

    Update Comparing joins.sql

--- JoiningDataInSQL/Comparing joins.sql
+++ JoiningDataInSQL/Comparing joins.sql
@@ -3,8 +3,8 @@ In this exercise, you'll examine how results can differ when performing a full j
 the countries and currencies tables. You'll be focusing on the North American region and records where the name of the country is missing.
 
 You'll begin with a full join with countries on the left and currencies on the right. 
-Recall the workings of a full join with the diagram below!
+Recall the workings of a full join with the diagram below! */
 
+JoiningDataInSQL/full_join2.png
 
-
-You'll then complete a similar left join and conclude with an inner join, observing the results you see along the way.  */
+/* You'll then complete a similar left join and conclude with an inner join, observing the results you see along the way.  */
