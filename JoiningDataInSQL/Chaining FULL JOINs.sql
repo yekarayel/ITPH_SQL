ff120e043119fa274ba14d817a115342311a92cf
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-06 00:18:16 +0100

    Update Chaining FULL JOINs.sql

--- JoiningDataInSQL/Chaining FULL JOINs.sql
+++ JoiningDataInSQL/Chaining FULL JOINs.sql
@@ -11,5 +11,19 @@ Instructions
 * Complete the FULL JOIN with countries as c1 on the left and languages as l on the right, using code to perform this join.
 * Next, chain this join with another FULL JOIN, placing currencies on the right, joining on code again.  */
 
+SELECT 
+	c1.name AS country, 
+    region, 
+    l.name AS language,
+	basic_unit, 
+    frac_unit
+FROM countries as c1 
+-- Full join with languages (alias as l)
+FULL JOIN languages AS l
+USING (code)
+-- Full join with currencies (alias as c2)
+FULL JOIN currencies AS c2
+USING (code)
+WHERE region LIKE 'M%esia';
 
 
