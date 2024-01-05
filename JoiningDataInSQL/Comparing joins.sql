becedee0a5dacacd0f756a6de2c966a0c24473c5
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-05 23:54:56 +0100

    Create Comparing joins.sql

---
+++ JoiningDataInSQL/Comparing joins.sql
@@ -0,0 +1,10 @@
+/* Comparing joins
+In this exercise, you'll examine how results can differ when performing a full join compared to a left join and inner join by joining 
+the countries and currencies tables. You'll be focusing on the North American region and records where the name of the country is missing.
+
+You'll begin with a full join with countries on the left and currencies on the right. 
+Recall the workings of a full join with the diagram below!
+
+
+
+You'll then complete a similar left join and conclude with an inner join, observing the results you see along the way.  */
