0082f70c3321d4084d80a3c8a1c8fbb612a562b4
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-06 00:15:56 +0100

    Create Chaining FULL JOINs.sql

---
+++ JoiningDataInSQL/Chaining FULL JOINs.sql
@@ -0,0 +1,15 @@
+/* Chaining FULL JOINs
+As you have seen in the previous chapter on INNER JOIN, it is possible to chain joins in SQL, such as when looking 
+to connect data from more than two tables.
+
+Suppose you are doing some research on Melanesia and Micronesia, and are interested in pulling information about languages and 
+currencies into the data we see for these regions in the countries table. Since languages and currencies exist in separate tables, 
+this will require two consecutive full joins involving the countries, languages and currencies tables.
+
+Instructions
+
+* Complete the FULL JOIN with countries as c1 on the left and languages as l on the right, using code to perform this join.
+* Next, chain this join with another FULL JOIN, placing currencies on the right, joining on code again.  */
+
+
+
