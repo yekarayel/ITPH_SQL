888dec61471d7ef0010810f2fa99dd3850a68d0f
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-09 23:37:06 +0100

    Create Histories and languages.sql

---
+++ JoiningDataInSQL/Histories and languages.sql
@@ -0,0 +1,11 @@
+/* Histories and languages
+Well done getting to know all about CROSS JOIN! As you have learned, CROSS JOIN can be incredibly helpful when asking questions that involve looking at all possible combinations or pairings between two sets of data.
+
+Imagine you are a researcher interested in the languages spoken in two countries: Pakistan and India. You are interested in asking:
+
+What are the languages presently spoken in the two countries?
+Given the shared history between the two countries, what languages could potentially have been spoken in either country over the course of their history?
+In this exercise, we will explore how INNER JOIN and CROSS JOIN can help us answer these two questions, respectively.  */
+
+
+
