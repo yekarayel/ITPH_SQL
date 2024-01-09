61b08b8bbd17c70c2f34c8b5307dd438d887544c
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-09 23:39:53 +0100

    Update Histories and languages.sql

--- JoiningDataInSQL/Histories and languages.sql
+++ JoiningDataInSQL/Histories and languages.sql
@@ -3,9 +3,15 @@ Well done getting to know all about CROSS JOIN! As you have learned, CROSS JOIN 
 
 Imagine you are a researcher interested in the languages spoken in two countries: Pakistan and India. You are interested in asking:
 
-What are the languages presently spoken in the two countries?
-Given the shared history between the two countries, what languages could potentially have been spoken in either country over the course of their history?
-In this exercise, we will explore how INNER JOIN and CROSS JOIN can help us answer these two questions, respectively.  */
+1. What are the languages presently spoken in the two countries?
+2. Given the shared history between the two countries, what languages could potentially have been spoken in either country over the course of their history?
+
+In this exercise, we will explore how INNER JOIN and CROSS JOIN can help us answer these two questions, respectively.  
+
+Instructions
+
+1- Complete the code to perform an INNER JOIN of countries AS c with languages AS l using the code field to obtain the languages 
+currently spoken in the two countries.  */
 
 
 
