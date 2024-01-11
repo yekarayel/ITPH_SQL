d94dfeb60cdfc5ebcbc3e568a6a14965237650e4
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-12 00:00:17 +0100

    Create Choosing your join.sql

---
+++ JoiningDataInSQL/Choosing your join.sql
@@ -0,0 +1,5 @@
+/* Choosing your join
+Now that you're fully equipped to use joins, try a challenge problem to test your knowledge!
+
+You will determine the names of the five countries and their respective regions with the lowest life expectancy for the year 2010. 
+Use your knowledge about joins, filtering, sorting and limiting to create this list! */
