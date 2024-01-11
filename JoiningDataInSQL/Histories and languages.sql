0bb052f703c9a11ed9adf913177447433544a321
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-11 23:51:30 +0100

    Update Histories and languages.sql

--- JoiningDataInSQL/Histories and languages.sql
+++ JoiningDataInSQL/Histories and languages.sql
@@ -29,3 +29,11 @@ WHERE c.code IN ('PAK','IND')
 	AND l.code in ('PAK','IND');
 
 -- 2 --------------------------------
+SELECT c.name AS country, l.name AS language
+FROM countries AS c        
+-- Perform a cross join to languages (alias as l)
+CROSS JOIN languages AS l 
+WHERE c.code in ('PAK','IND')
+	AND l.code in ('PAK','IND');
+
+
