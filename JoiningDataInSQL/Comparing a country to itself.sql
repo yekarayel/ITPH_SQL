abce9ab055edaa6cd9dc012ed1221928a625e202
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-12 22:29:29 +0100

    Update Comparing a country to itself.sql

--- JoiningDataInSQL/Comparing a country to itself.sql
+++ JoiningDataInSQL/Comparing a country to itself.sql
@@ -14,3 +14,15 @@ Instructions
     p2.size as size2015 (in that order).
 */
 
+-- 1 --------------------------------------
+-- Select aliased fields from populations as p1
+SELECT  p1.country_code, 
+        p1.size AS size2010,
+        p2.size AS size2015
+-- Join populations as p1 to itself, alias as p2, on country code
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code;
+
+
+-- 2 --------------------------------------
