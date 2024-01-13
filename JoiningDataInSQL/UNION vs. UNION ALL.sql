36c7dd13334b373e49636131df3c5717240e318b
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-13 23:56:46 +0100

    Create UNION vs. UNION ALL.sql

---
+++ JoiningDataInSQL/UNION vs. UNION ALL.sql
@@ -0,0 +1,8 @@
+/* UNION vs. UNION ALL
+Nice work learning all about UNION and UNION ALL!
+
+Two tables, languages and currencies, are provided. Run the queries provided in the console and select the correct answer for 
+the multiple-choice questions in this exercise.
+
+
+*/
