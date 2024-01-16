9de8394ef1848995cba1af7d13b47175f4bff4c5
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-17 00:07:50 +0100

    Update UNION vs. UNION ALL.sql

--- JoiningDataInSQL/UNION vs. UNION ALL.sql
+++ JoiningDataInSQL/UNION vs. UNION ALL.sql
@@ -14,14 +14,56 @@ FROM languages
 UNION
 SELECT * 
 FROM currencies;
-Possible answers
+
+*** Possible answers ***
+
+o - All records from both tables, dropping duplicate records (if any)
+
+X - A SQL error, because languages and currencies do not have the same number of fields
+
+o - A SQL error, because languages and currencies do not have the same number of records
 
 
-o All records from both tables, dropping duplicate records (if any)
 
-o A SQL error, because languages and currencies do not have the same number of fields
+2- Question
+What result will the following SQL query produce?
 
-o A SQL error, because languages and currencies do not have the same number of records
+SELECT code FROM
+languages
+UNION ALL
+SELECT code FROM 
+currencies;
+
+*** Possible answers ***
+
+o - An ordered list of each country code in languages and currencies, including duplicates
+
+o - An ordered list of each unique country code in languages and currencies
+
+X - An unordered list of each country code in languages and currencies, including duplicates
+
+o - An unordered list of each unique country code in languages and currencies
+
+
+
+3 -Question
+What will the following SQL query produce?
+
+SELECT code 
+FROM languages
+UNION
+SELECT curr_id 
+FROM currencies;
+
+*** Possible answers ***
+
+o - An empty result
+
+o - A stacked list of every curr_id from currencies and every code from languages
+
+X - A SQL error, because code and curr_id are not of the same data type
+
+o - A SQL error, because code and curr_id do not have the same name
 
 
 */
