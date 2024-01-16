bebaba62ead3f718e71c621bbe5452332e317b3f
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-16 23:58:26 +0100

    Update UNION vs. UNION ALL.sql

--- JoiningDataInSQL/UNION vs. UNION ALL.sql
+++ JoiningDataInSQL/UNION vs. UNION ALL.sql
@@ -4,5 +4,24 @@ Nice work learning all about UNION and UNION ALL!
 Two tables, languages and currencies, are provided. Run the queries provided in the console and select the correct answer for 
 the multiple-choice questions in this exercise.
 
+Instructions
+
+1- Question
+What result will the following SQL query produce?
+
+SELECT * 
+FROM languages
+UNION
+SELECT * 
+FROM currencies;
+Possible answers
+
+
+o All records from both tables, dropping duplicate records (if any)
+
+o A SQL error, because languages and currencies do not have the same number of fields
+
+o A SQL error, because languages and currencies do not have the same number of records
+
 
 */
