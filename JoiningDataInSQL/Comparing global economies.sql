ec50da12ff819bce85248028739481f4756bdece
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-17 14:35:16 +0100

    Update Comparing global economies.sql

--- JoiningDataInSQL/Comparing global economies.sql
+++ JoiningDataInSQL/Comparing global economies.sql
@@ -12,3 +12,14 @@ Instructions
 * Begin your query by selecting all fields from economies2015.
 * Create a second query that selects all fields from economies2019.
 * Perform a set operation to combine the two queries you just created, ensuring you do not return duplicates.  */
+
+
+-- Select all fields from economies2015
+SELECT *
+FROM economies2015
+-- Set operation
+UNION
+-- Select all fields from economies2019
+SELECT *
+FROM economies2019
+ORDER BY code, year;
