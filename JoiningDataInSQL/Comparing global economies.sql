cc0ae61bf483ee67fe8c23e5dcddcefb9d24adf3
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-17 14:33:32 +0100

    Create Comparing global economies.sql

---
+++ JoiningDataInSQL/Comparing global economies.sql
@@ -0,0 +1,14 @@
+/* Comparing global economies
+Are you ready to perform your first set operation?
+
+In this exercise, you have two tables, economies2015 and economies2019, available to you under the tabs in the console. You'll perform 
+a set operation to stack all records in these two tables on top of each other, excluding duplicates.
+
+When drafting queries containing set operations, it is often helpful to write the queries on either side of the operation first, 
+and then call the set operator. The instructions are ordered accordingly.
+
+Instructions
+
+* Begin your query by selecting all fields from economies2015.
+* Create a second query that selects all fields from economies2019.
+* Perform a set operation to combine the two queries you just created, ensuring you do not return duplicates.  */
