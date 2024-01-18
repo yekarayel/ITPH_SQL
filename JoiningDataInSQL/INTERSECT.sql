b870c1c5d106af6bcaf996f74715ee7d75bf34cf
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-18 01:07:12 +0100

    Create INTERSECT.sql

---
+++ JoiningDataInSQL/INTERSECT.sql
@@ -0,0 +1,9 @@
+/* INTERSECT
+Well done getting through the material on INTERSECT!
+
+Let's say you are interested in those countries that share names with cities. Use this task as an opportunity to show off your 
+knowledge of set theory in SQL!
+
+Instructions
+
+Return all city names that are also country names.  */
