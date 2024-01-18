1b9e32c7e80836805c21ce95f5997170da287aca
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-18 01:59:50 +0100

    Update You've got it, EXCEPT...sql

--- JoiningDataInSQL/You've got it, EXCEPT...sql
+++ JoiningDataInSQL/You've got it, EXCEPT...sql
@@ -9,4 +9,16 @@ Instructions
 Return all cities that do not have the same name as a country.  */
 
 
+-- Return all cities that do not have the same name as a country
+SELECT name
+FROM cities
 
+EXCEPT
+
+SELECT name
+FROM countries
+
+ORDER BY name;
+
+
+--
