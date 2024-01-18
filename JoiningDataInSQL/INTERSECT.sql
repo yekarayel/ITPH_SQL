eeaed9c22b900caabf007077b81e9a12f7ec2bc5
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-18 01:56:30 +0100

    Update INTERSECT.sql

--- JoiningDataInSQL/INTERSECT.sql
+++ JoiningDataInSQL/INTERSECT.sql
@@ -7,3 +7,10 @@ knowledge of set theory in SQL!
 Instructions
 
 Return all city names that are also country names.  */
+
+-- Return all cities with the same name as a country
+SELECT name
+FROM cities
+INTERSECT
+SELECT name
+FROM countries;
