95cec5c43d25d3af24630896da58cb372302976c
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-18 01:58:35 +0100

    Create You've got it, EXCEPT...sql

---
+++ JoiningDataInSQL/You've got it, EXCEPT...sql
@@ -0,0 +1,12 @@
+/*  You've got it, EXCEPT...
+Just as you were able to leverage INTERSECT to find the names of cities with the same names as countries, you can also do the reverse, 
+using EXCEPT.
+
+In this exercise, you will find the names of cities that do not have the same names as their countries.
+
+Instructions
+
+Return all cities that do not have the same name as a country.  */
+
+
+
