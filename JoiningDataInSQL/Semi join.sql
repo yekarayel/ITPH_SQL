140474b1e822cc3392e15dc410f486bc2297a577
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-01-19 23:27:29 +0100

    Create Semi join.sql

---
+++ JoiningDataInSQL/Semi join.sql
@@ -0,0 +1,14 @@
+/* Semi join
+Great job getting acquainted with semi joins and anti joins! You are now going to practice using semi joins.
+
+Let's say you are interested in identifying languages spoken in the Middle East. The languages table contains information about 
+languages and countries, but it does not tell you what region the countries belong to. You can build up a semi join by filtering 
+the countries table by a particular region, and then using this to further filter the languages table.
+
+You'll build up your semi join as you did in the video exercise, block by block, starting with a selection of countries from 
+the countries table, and then leveraging a WHERE clause to filter the languages table by this selection.
+
+Instructions
+
+1- Select country code as a single field from the countries table, filtering for countries in the 'Middle East' region.  */
+
