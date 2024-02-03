559fad2f28aa718abe85d46a9da9e49aabaf60e3
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-02-04 00:38:42 +0100

    Update Semi join.sql

--- JoiningDataInSQL/Semi join.sql
+++ JoiningDataInSQL/Semi join.sql
@@ -13,7 +13,10 @@ Instructions
 1- Select country code as a single field from the countries table, filtering for countries in the 'Middle East' region.  
 
 2- * Write a second query to SELECT the name of each unique language appearing in the languages table; do not use column aliases here.
-   * Order the result set by name in ascending order.*/
+   * Order the result set by name in ascending order.
+
+3- Create a semi join out of the two queries you've written, which filters unique languages returned in the first query for only those 
+languages spoken in the 'Middle East'.  */
 
 
 -- 1 -----------------------------------
@@ -29,3 +32,16 @@ SELECT DISTINCT name
 FROM languages
 -- Order by the name of the language
 ORDER BY name;
+
+
+-- 3 -----------------------------------
+SELECT DISTINCT name
+FROM languages
+-- Add syntax to use bracketed subquery below as a filter
+WHERE code IN
+    (SELECT code
+    FROM countries
+    WHERE region = 'Middle East')
+ORDER BY name;
+
+-
