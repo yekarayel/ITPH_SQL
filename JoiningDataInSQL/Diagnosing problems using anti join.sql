2c83b8a5768964ed6d3d675ea333b338b7479640
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-02-04 01:41:33 +0100

    Update Diagnosing problems using anti join.sql

--- JoiningDataInSQL/Diagnosing problems using anti join.sql
+++ JoiningDataInSQL/Diagnosing problems using anti join.sql
@@ -20,7 +20,8 @@ Instructions
 the countries table.
    * Observe the number of records returned and compare this with the provided INNER JOIN, which returns 15 records.
 
-*/
+2- Now, build on your query to complete your anti join, by adding an additional filter to return every country code that is not 
+included in the currencies table.  */
 
 
 -- 1 ---------------------------------------
@@ -28,3 +29,13 @@ the countries table.
 SELECT code, name 
 FROM countries
 WHERE continent = 'Oceania' ;
+
+
+-- 2 ---------------------------------------
+SELECT code, name
+FROM countries
+WHERE continent = 'Oceania'
+-- Filter for countries not included in the bracketed subquery
+  AND code NOT IN 
+    (SELECT code
+    FROM currencies);
