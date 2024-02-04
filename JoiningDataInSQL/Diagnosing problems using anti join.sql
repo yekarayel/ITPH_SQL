36097f6003954a1b738ae882eb9fc4dd5dc4b681
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-02-04 01:35:51 +0100

    Create Diagnosing problems using anti join.sql

---
+++ JoiningDataInSQL/Diagnosing problems using anti join.sql
@@ -0,0 +1,30 @@
+/* Diagnosing problems using anti join
+Nice work on semi joins! The anti join is a related and powerful joining tool. It can be particularly useful for identifying whether 
+an incorrect number of records appears in a join.
+
+Say you are interested in identifying currencies of Oceanian countries. You have written the following INNER JOIN, which returns 15 records.
+Now, you want to ensure that all Oceanian countries from the countries table are included in this result. You'll do this in the first step.
+
+SELECT c1.code, name, basic_unit AS currency
+FROM countries AS c1
+INNER JOIN currencies AS c2
+ON c1.code = c2.code
+WHERE c1.continent = 'Oceania';
+
+If there are any Oceanian countries excluded in this INNER JOIN, you want to return the names of these countries. You'll write an anti join
+to this in the second step!  
+
+Instructions 
+
+1- * Begin by writing a query to return the code and name (in order, not aliased) for all countries in the continent of Oceania from 
+the countries table.
+   * Observe the number of records returned and compare this with the provided INNER JOIN, which returns 15 records.
+
+*/
+
+
+-- 1 ---------------------------------------
+-- Select code and name of countries from Oceania
+SELECT code, name 
+FROM countries
+WHERE continent = 'Oceania' ;
