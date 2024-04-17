9bd2d9d7454c612daf105b628e256294dcda4c73
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2024-04-18 00:17:52 +0200

    Update Subquery inside FROM.sql

--- JoiningDataInSQL/Subquery inside FROM.sql
+++ JoiningDataInSQL/Subquery inside FROM.sql
@@ -7,6 +7,31 @@ alongside each country's local_name, which is a field only present in the countr
 You'll use a subquery inside FROM to bring information from these two tables together!
 
 Instructions 
-1 -  * Begin with a query that groups by each country code from languages, and counts the languages spoken in each country as lang_num.
-     * In your SELECT statement, return code and lang_num (in that order). */
 
+1 -  * Begin with a query that groups by each country code from languages, and counts the languages spoken in each country as lang_num.
+     * In your SELECT statement, return code and lang_num (in that order). 
+
+2 -  * Select local_name from countries, with the aliased lang_num from your subquery (which has been nested and aliased for you as sub).
+     * Use WHERE to match the code field from countries and sub.    */
+
+
+-- 1 -----------------------------
+-- Select code, and language count as lang_num
+SELECT code, COUNT(name) AS lang_num
+FROM languages 
+GROUP BY code;
+
+
+-- 2 -----------------------------
+-- Select local_name and lang_num from appropriate tables
+SELECT countries.local_name,sub.lang_num
+FROM countries,
+  (SELECT code, COUNT(*) AS lang_num
+  FROM languages
+  GROUP BY code) AS sub
+-- Where codes match
+WHERE countries.code = sub.code
+ORDER BY lang_num DESC;
+
+
+--
